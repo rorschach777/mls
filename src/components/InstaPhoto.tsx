61e388e24922c29dd7b0d94733d30c1e135d7108
Mark Sweitzer <msweitzer1987@gmail.com>
2024-05-15 22:04:53 -0400

    gallery sizing fixed

--- src/components/InstaPhoto.tsx
+++ src/components/InstaPhoto.tsx
@@ -1,120 +1,136 @@
 
 import './InstaPhoto.css';
-const baseUrl = "";
+const baseUrl = "https://i.imgur.com/";
+import { Image } from 'antd';
 
 const dummyImageData = {
 
     imageRows: [
         {
-            name: 'row1', 
-            image1:  {
+            name: 'row1',
+            image1: {
                 meta: 'Image 1',
-                url: `${baseUrl}/1.jpg`
-            }, 
-            image2:  {
+                url: `${baseUrl}/FOXwyne.jpeg`
+            },
+            image2: {
                 meta: 'Image 2',
-                url: `${baseUrl}/2.jpg`
-            }, 
-            image3:  {
+                url: `${baseUrl}/6DBGPk5.jpeg`
+            },
+            image3: {
                 meta: 'Image 3',
-                url: `${baseUrl}/3.jpg`
-            }, 
-            image4:  {
+                url: `${baseUrl}/DA00KOH.jpeg`
+            },
+            image4: {
                 meta: 'Image 4',
-                url: `${baseUrl}/4.jpg`
-            }, 
+                url: `${baseUrl}/FOXwyne.jpeg`
+            },
         },
         {
-            name: 'row2', 
-            image1:  {
+            name: 'row2',
+            image1: {
                 meta: 'Image 1',
-                url: `${baseUrl}/5.jpg`
-            }, 
-            image2:  {
+                url: `${baseUrl}/FOXwyne.jpeg`
+            },
+            image2: {
                 meta: 'Image 2',
-                url: `${baseUrl}/6.jpg`
-            }, 
-            image3:  {
+                url: `${baseUrl}/FOXwyne.jpeg`
+            },
+            image3: {
                 meta: 'Image 3',
-                url: `${baseUrl}/7.jpg`
-            }, 
-            image4:  {
+                url: `${baseUrl}/FOXwyne.jpeg`
+            },
+            image4: {
                 meta: 'Image 4',
-                url: `${baseUrl}/8.jpg`
-            }, 
+                url: `${baseUrl}/FOXwyne.jpeg`
+            },
         },
         {
-            name: 'row2', 
-            image1:  {
+            name: 'row2',
+            image1: {
                 meta: 'Image 1',
-                url: `${baseUrl}/9.jpg`
-            }, 
-            image2:  {
+                url: `${baseUrl}/FOXwyne.jpeg`
+            },
+            image2: {
                 meta: 'Image 2',
-                url: `${baseUrl}/10.jpg`
-            }, 
-            image3:  {
+                url: `${baseUrl}/FOXwyne.jpeg`
+            },
+            image3: {
                 meta: 'Image 3',
-                url: `${baseUrl}/11.jpg`
-            }, 
-            image4:  {
+                url: `${baseUrl}/FOXwyne.jpeg`
+            },
+            image4: {
                 meta: 'Image 4',
-                url: `${baseUrl}/12.jpg`
-            }, 
+                url: `${baseUrl}/FOXwyne.jpeg`
+            },
         }
     ]
 }
-interface ImageObj  {
+interface ImageObj {
     meta: String,
     url: String
 }
 interface ImageRow {
     name: string,
-    image1 : ImageObj,
+    image1: ImageObj,
     image2: ImageObj
     image3: ImageObj,
     image4: ImageObj
 }
 interface ImageRows {
-    imageRows : Array<ImageRow>
+    imageRows: Array<ImageRow>
 }
 
 const generateInstaRow = () => {
-    let data : ImageRows= dummyImageData ;
-    
-    return data.imageRows.map((c : ImageRow ,i : number)=>{
+    let data: ImageRows = dummyImageData;
+
+    return data.imageRows.map((c: ImageRow, i: number) => {
         return (
             <div className="insta-grid-row">
-            <div className="insta-grid-column" style={{backgroundImage:`url(${c.image1.url})`}}></div>
-            <div className="insta-grid-column">
-                <div className="insta-grid-mosaic">
-                    <div className="insta-grid-mosaic-container">
-                        <div className="insta-grid-mosaic-item" style={{backgroundImage:`url(${c.image2.url})`}}>
+                <div className="insta-grid-column">
+                    <Image
+                        src={`${c.image1.url}`}
+                    />
+                </div>
+                <div className="insta-grid-column">
+                    <div className="insta-grid-mosaic">
+                        <div className="insta-grid-mosaic-container">
+
+                            <div className="insta-grid-mosaic-item">
+                            <Image
+                                src={`${c.image2.url}`}
+                            />
+                            </div>
+                            <div className="insta-grid-mosaic-item" >
+                            <Image
+                                src={`${c.image3.url}`}
+                            />
+                            </div>
                         </div>
-                        <div className="insta-grid-mosaic-item" style={{backgroundImage:`url(${c.image3.url})`}}>
+                        <div className='insta-grid-mosaic-container'>
+                            <div className="insta-grid-mosaic-item">
+                                <Image
+                                    src={`${c.image2.url}`}
+                                />
+                            </div>
                         </div>
-                    </div>
-                    <div className='insta-grid-mosaic-container'>
-                        <div className="insta-grid-mosaic-item" style={{backgroundImage:`url(${c.image4.url})`}}></div>
                     </div>
                 </div>
             </div>
-        </div>
         );
     })
 
 }
 
 
-export default function InstaPhoto(){
+export default function InstaPhoto() {
     return (
         <div className="insta-grid">
             {generateInstaRow()}
-  
-            
+
+
         </div>
     );
-    
+
 }
 
 
